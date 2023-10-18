5eb76c98303183983cc119ca728925e9e2a3bbee
CarlosDanyel <carlosdanyelsilva27@gmail.com>
2023-10-18 18:57:04 -0300

    add rota img

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import noticias from "./noticiasRoutes.js";
-
 import cors from "cors";
+import path from "path";
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const tempFolder = path.resolve(__dirname, "../..", "public");
 const routes = (app) => {
 
-    app.use("/files", express.static("public/"));
+    app.use("/files", express.static(tempFolder));
     
     app.use((req, res, next) => {
-        res.header("Access-Control-Allow-OrigIn", "https://portfolio-carlosdanyel.vercel.app");
+        res.header("Access-Control-Allow-OrigIn", "*");
         res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
         res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
         app.use(cors());
